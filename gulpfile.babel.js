f3b066b49f7f01a13aca8c70ab4ea3113f320a3f
Christian Burkhart <burkhart.christian@gmail.com>
2016-07-23 14:08:41 +0200

    set up project

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -29,6 +29,7 @@ import gulp from 'gulp';
 import del from 'del';
 import runSequence from 'run-sequence';
 import browserSync from 'browser-sync';
+import include from 'gulp-include';
 import swPrecache from 'sw-precache';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import {output as pagespeed} from 'psi';
@@ -90,7 +91,8 @@ gulp.task('styles', () => {
     .pipe($.newer('.tmp/styles'))
     .pipe($.sourcemaps.init())
     .pipe($.sass({
-      precision: 10
+      precision: 10,
+      includePaths: ['node_modules/foundation-sites/scss']
     }).on('error', $.sass.logError))
     .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
     .pipe(gulp.dest('.tmp/styles'))
@@ -196,7 +198,7 @@ gulp.task('default', ['clean'], cb =>
   runSequence(
     'styles',
     ['lint', 'html', 'scripts', 'images', 'copy'],
-    'generate-service-worker',
+    // 'generate-service-worker',
     cb
   )
 );
@@ -223,31 +225,31 @@ gulp.task('copy-sw-scripts', () => {
 // Generate a service worker file that will provide offline functionality for
 // local resources. This should only be done for the 'dist' directory, to allow
 // live reload to work as expected when serving from the 'app' directory.
-gulp.task('generate-service-worker', ['copy-sw-scripts'], () => {
-  const rootDir = 'dist';
-  const filepath = path.join(rootDir, 'service-worker.js');
+// gulp.task('generate-service-worker', ['copy-sw-scripts'], () => {
+//   const rootDir = 'dist';
+//   const filepath = path.join(rootDir, 'service-worker.js');
 
-  return swPrecache.write(filepath, {
-    // Used to avoid cache conflicts when serving on localhost.
-    cacheId: pkg.name || 'web-starter-kit',
-    // sw-toolbox.js needs to be listed first. It sets up methods used in runtime-caching.js.
-    importScripts: [
-      'scripts/sw/sw-toolbox.js',
-      'scripts/sw/runtime-caching.js'
-    ],
-    staticFileGlobs: [
-      // Add/remove glob patterns to match your directory setup.
-      `${rootDir}/images/**/*`,
-      `${rootDir}/scripts/**/*.js`,
-      `${rootDir}/styles/**/*.css`,
-      `${rootDir}/*.{html,json}`
-    ],
-    // Translates a static file path to the relative URL that it's served from.
-    // This is '/' rather than path.sep because the paths returned from
-    // glob always use '/'.
-    stripPrefix: rootDir + '/'
-  });
-});
+//   return swPrecache.write(filepath, {
+//     // Used to avoid cache conflicts when serving on localhost.
+//     cacheId: pkg.name || 'web-starter-kit',
+//     // sw-toolbox.js needs to be listed first. It sets up methods used in runtime-caching.js.
+//     importScripts: [
+//       'scripts/sw/sw-toolbox.js',
+//       'scripts/sw/runtime-caching.js'
+//     ],
+//     staticFileGlobs: [
+//       // Add/remove glob patterns to match your directory setup.
+//       `${rootDir}/images/**/*`,
+//       `${rootDir}/scripts/**/*.js`,
+//       `${rootDir}/styles/**/*.css`,
+//       `${rootDir}/*.{html,json}`
+//     ],
+//     // Translates a static file path to the relative URL that it's served from.
+//     // This is '/' rather than path.sep because the paths returned from
+//     // glob always use '/'.
+//     stripPrefix: rootDir + '/'
+//   });
+// });
 
 // Load custom tasks from the `tasks` directory
 // Run: `npm install --save-dev require-dir` from the command-line
